17e4a5f3afe9da7cfb8dd47420b2cf0856222a6c
Varun-Jajoo <128844637+Varun-Jajoo@users.noreply.github.com>
2023-04-25 17:29:35 +0530

    Update insertionsort.c

--- insertionsort.c
+++ insertionsort.c
@@ -1,44 +1,50 @@
 #include <stdio.h>
 
-void insertionSort(int arr[], int size);
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+
+        // Printing array after every pass
+        printf("Pass %d: ", i);
+        for (int k = 0; k < n; k++) {
+            printf("%d ", arr[k]);
+        }
+        printf("\n");
+    }
+}
 
 int main() {
-    int size;
-    printf("Enter the number of elements: ");
-    scanf("%d", &size);
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
 
-    int arr[size];
-    printf("Enter the elements:\n");
-    for (int i = 0; i < size; i++) {
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    insertionSort(arr, size);  // sort the array using the insertion sort algorithm
+    printf("Original array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 
-    printf("The sorted array is:\n");
-    for (int i = 0; i < size; i++) {
+    insertionSort(arr, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 
     return 0;
-}
-
-// function to perform the insertion sort algorithm
-void insertionSort(int arr[], int size) {
-    int i, j, key;
-
-    // loop through the array starting from the second element
-    for (i = 1; i < size; i++) {
-        key = arr[i];  // store the current element in a variable
-
-        // move all elements greater than the current element one position to the right
-        j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-
-        arr[j + 1] = key;  // insert the current element into the correct position
-    }
 }
