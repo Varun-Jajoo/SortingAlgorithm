c33e47c10d96ad7a877b4d40479f56094882a780
Varun-Jajoo <128844637+Varun-Jajoo@users.noreply.github.com>
2023-04-25 17:36:56 +0530

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -1,71 +1,42 @@
 #include <stdio.h>
 
-// function to merge two sorted subarrays into a single sorted subarray
-void merge(int arr[], int left[], int leftSize, int right[], int rightSize) {
+int num_calls = 0;
+
+void merge(int arr[], int left[], int left_size, int right[], int right_size) {
     int i = 0, j = 0, k = 0;
-
-    // compare elements from the left and right subarrays and merge them into a single sorted subarray
-    while (i < leftSize && j < rightSize) {
-        if (left[i] <= right[j]) {
-            arr[k++] = left[i++];
-        } else {
-            arr[k++] = right[j++];
-        }
+    while (i < left_size && j < right_size) {
+        arr[k++] = (left[i] <= right[j]) ? left[i++] : right[j++];
     }
-
-    // copy any remaining elements from the left subarray into the merged subarray
-    while (i < leftSize) {
+    while (i < left_size) {
         arr[k++] = left[i++];
     }
-
-    // copy any remaining elements from the right subarray into the merged subarray
-    while (j < rightSize) {
+    while (j < right_size) {
         arr[k++] = right[j++];
     }
 }
 
-// function to perform the merge sort algorithm
-void mergeSort(int arr[], int size) {
-    if (size < 2) {  // if the size of the array is less than 2, it is already sorted
-        return;
-    }
-
-    int mid = size / 2;
-
-    int left[mid];  // create a new array to store the left subarray
-    for (int i = 0; i < mid; i++) {
-        left[i] = arr[i];
-    }
-
-    int right[size - mid];  // create a new array to store the right subarray
-    for (int i = mid; i < size; i++) {
-        right[i - mid] = arr[i];
-    }
-
-    mergeSort(left, mid);  // recursively sort the left subarray
-    mergeSort(right, size - mid);  // recursively sort the right subarray
-
-    merge(arr, left, mid, right, size - mid);  // merge the sorted left and right subarrays into a single sorted subarray
+void merge_sort(int arr[], int n) {
+    if (n <= 1) return;
+    int mid = n / 2;
+    int left[mid], right[n - mid];
+    for (int i = 0; i < mid; i++) left[i] = arr[i];
+    for (int i = mid; i < n; i++) right[i - mid] = arr[i];
+    merge_sort(left, mid);
+    merge_sort(right, n - mid);
+    merge(arr, left, mid, right, n - mid);
+    num_calls++;
 }
 
 int main() {
-    int size;
+    int n;
     printf("Enter the number of elements: ");
-    scanf("%d", &size);
-
-    int arr[size];
-    printf("Enter the elements:\n");
-    for (int i = 0; i < size; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    mergeSort(arr, size);  // sort the array using the merge sort algorithm
-
-    printf("The sorted array is:\n");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    merge_sort(arr, n);
+    printf("Sorted elements: ");
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\nNumber of calls to merge_sort(): %d\n", num_calls);
     return 0;
 }
