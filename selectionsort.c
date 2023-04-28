8b860a817362da41007198701e6158177f95f15c
Varun-Jajoo <128844637+Varun-Jajoo@users.noreply.github.com>
2023-04-28 17:19:44 +0530

    Update selectionsort.c

--- selectionsort.c
+++ selectionsort.c
@@ -1,63 +1,40 @@
-#include <stdio.h>
+#include<stdio.h>
 
-// Function to swap two elements
-void swap(int *xp, int *yp) {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
+int main()
+{
+   int small,i,j,k,temp,a[30],n;
+   printf("Enter Array Size: \n");
+   scanf("%d",&n);
+   printf("Enter elements one by one \n");
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   printf("Unsorted array is:\n");
+   for(i=0;i<n;i++)
+   {
+       printf("%d\t",a[i]);
+   }
 
-// Function to perform selection sort
-void selectionSort(int arr[], int n) {
-    int i, j, min_idx;
-    
-    // Traverse through all elements in the array
-    for (i = 0; i < n-1; i++) {
-        min_idx = i; // Assume the first element as the minimum
-        
-        // Find the minimum element in the remaining unsorted array
-        for (j = i+1; j < n; j++) {
-            if (arr[j] < arr[min_idx]) {
-                min_idx = j; // Update the minimum index
-            }
-        }
-        
-        // Swap the found minimum element with the first element
-        swap(&arr[min_idx], &arr[i]);
-        
-        // Print the array after every pass
-        printf("Pass %d: ", i+1);
-        for (j = 0; j < n; j++) {
-            printf("%d ", arr[j]);
-        }
-        printf("\n");
-    }
-}
+   for(i=0;i<n;i++)
+   {
+       small=i;
+       for(j=i+1;j<n;j++)
+       {
+           if(a[j]<a[small])
+            small=j;
+       }
 
-int main() {
-    int n;
-    printf("Enter the number of elements in the array: ");
-    scanf("%d", &n); // Input the number of elements
-    
-    int arr[n];
-    printf("Enter the elements of the array: ");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]); // Input the array elements
-    }
-    
-    printf("Original array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]); // Print the original array
-    }
-    printf("\n");
-    
-    selectionSort(arr, n); // Call selectionSort function to sort the array
-    
-    printf("Sorted array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]); // Print the sorted array
-    }
-    printf("\n");
-    
-    return 0;
+       temp=a[i];
+       a[i]=a[small];
+       a[small]=temp;
+
+       printf("\nSorted Array after %d pass: \n",i+1);
+       for(k=0;k<n;k++)
+       {
+        printf("%d\t",a[k]);
+       }
+
+   }
+
 }
